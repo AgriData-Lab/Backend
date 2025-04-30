57b8341ea8821b6a201171dec15e6c1ee0221c66
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-04-30 23:21:28 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/agridata/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package agridata.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
